daedalus-labs/filament-dryer

commit 87d7b5dc11115093156c54d9a264e334a86f2e2c

Update heater status value (#17)
This commit changes the value of `container/heater` from an integer
to a string.

Closes #16

src/controllers/heater.hpp
@@ -5,6 +5,7 @@ SPDX-License-Identifier: BSD-3-Clause
 #pragma once
 
 #include <cstdint>
+#include <string_view>
 
 
 namespace controllers {
@@ -14,6 +15,12 @@ namespace controllers {
 class Heater
 {
 public:
+    /** Human readable string for the Heater ON state. */
+    static constexpr std::string_view ON = "on";
+
+    /** Human readable string for the Heater OFF state. */
+    static constexpr std::string_view OFF = "off";
+
     /**
      * Constructor.
      *

src/main.cpp
@@ -23,6 +23,7 @@ SPDX-License-Identifier: BSD-3-Clause
 #include <cstdint>
 #include <cstdio>
 #include <functional>
+#include <string_view>
 
 
 inline constexpr float HEATER_HYSTERESIS = 2.5f;
@@ -105,7 +106,12 @@ static void publish(mqtt::Client& client, const feedback_entry& data)
     mqtt::publish(client, mqtt_topic, container_temperature);
 
     snprintf(mqtt_topic, TOPIC_BUFFER_SIZE, HEATER_TOPIC_FORMAT.data(), client.deviceName().c_str());
-    mqtt::publish(client, mqtt_topic, static_cast<uint32_t>(data.heater_on));
+    if (data.heater_on) {
+        mqtt::publish(client, mqtt_topic, controllers::Heater::ON.data());
+    }
+    else {
+        mqtt::publish(client, mqtt_topic, controllers::Heater::OFF.data());
+    }
 
     snprintf(mqtt_topic, TOPIC_BUFFER_SIZE, TARGET_TEMPERATURE_TOPIC_FORMAT.data(), client.deviceName().c_str());
     mqtt::publish(client, mqtt_topic, target_temperature);

commit 210673eeb12d470d371d208083b79037a7e5acd1

Fix build issues
This commit addresses the build issues introduced with #17

src/connectivity/constants.hpp
@@ -23,8 +23,6 @@ inline constexpr std::string_view UNKNOWN_IP = "-";
 
 namespace mqtt {
 inline constexpr uint16_t KEEP_ALIVE_TIMEOUT_S = 120;
-inline constexpr uint8_t OFF = 0;
-inline constexpr uint8_t ON = 1;
 inline constexpr uint8_t CONNECTED = ON;
 inline constexpr uint8_t DISCONNECTED = OFF;
 } // namespace mqtt

src/controllers/heater.hpp
@@ -16,10 +16,10 @@ class Heater
 {
 public:
     /** Human readable string for the Heater ON state. */
-    static constexpr std::string_view ON = "on";
+    static constexpr std::string_view STATUS_ON = "on";
 
     /** Human readable string for the Heater OFF state. */
-    static constexpr std::string_view OFF = "off";
+    static constexpr std::string_view STATUS_OFF = "off";
 
     /**
      * Constructor.

src/main.cpp
@@ -107,10 +107,10 @@ static void publish(mqtt::Client& client, const feedback_entry& data)
 
     snprintf(mqtt_topic, TOPIC_BUFFER_SIZE, HEATER_TOPIC_FORMAT.data(), client.deviceName().c_str());
     if (data.heater_on) {
-        mqtt::publish(client, mqtt_topic, controllers::Heater::ON.data());
+        mqtt::publish(client, mqtt_topic, controllers::Heater::STATUS_ON.data());
     }
     else {
-        mqtt::publish(client, mqtt_topic, controllers::Heater::OFF.data());
+        mqtt::publish(client, mqtt_topic, controllers::Heater::STATUS_OFF.data());
     }
 
     snprintf(mqtt_topic, TOPIC_BUFFER_SIZE, TARGET_TEMPERATURE_TOPIC_FORMAT.data(), client.deviceName().c_str());